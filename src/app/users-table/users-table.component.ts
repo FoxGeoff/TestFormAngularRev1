ff27830ac6f68290679ebb44b2a2e4ee5d2f5c3d
Geoff Fox <fox.geoffrey@gmail.com>
2021-07-22 15:05:40 -0700

    Added getter/setter on @Output()

--- src/app/users-table/users-table.component.ts
+++ src/app/users-table/users-table.component.ts
@@ -13,10 +13,20 @@ import { MatTable } from '@angular/material/table';
   styleUrls: ['./users-table.component.css']
 })
 export class UsersTableComponent implements OnInit, AfterViewInit {
-  @Input('userData') dataSource!: User[];
+
+  // https://angular.io/guide/component-interaction#intercept-input-property-changes-with-a-setter
+  private _userData: User[];
+  @Input('userData')
+  set userData(value: User[]) {
+    this._userData = value;
+    console.log(`set userdata`, this._userData)
+    addTableRow( this._userData[this._userData.length-1])
+  }
+  get userData(): User[] { return this._userData; }
 
   // Mat Table properties
   displayedColumns: string[] = ['firstName', 'lastName', 'email'];
+  dataSource = this.userData;
   @ViewChild(MatTable, { static: true }) table: MatTable<any>;
 
   constructor() {
@@ -27,7 +37,17 @@ export class UsersTableComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+    this.dataSource = this.userData;
     // this.table.renderRows();
   }
 
 }
+// TODO use async code FIX REQUIRED!!!
+function addTableRow(user: User) {
+  // debug code
+  console.log(`addTableRow(): `, user)
+
+  this.dataSource.push(user.firstName, user.lastName, user.email);
+  this.table.renderRows();
+}
+
